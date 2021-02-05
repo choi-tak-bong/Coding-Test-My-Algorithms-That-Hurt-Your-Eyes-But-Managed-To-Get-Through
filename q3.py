abf4b5a926fc2af1d683a348cdbd3dc2204df476
choi-tak-bong <78477426+choi-tak-bong@users.noreply.github.com>
2021-02-05 16:31:34 +0900

    Create q3.py

---
+++ q3.py
@@ -0,0 +1,15 @@
+from bisect import bisect_left, bisect_right
+
+string = input()
+
+zero_split = string.split("0")
+one_split = string.split("1")
+
+zero_split.sort()
+one_split.sort()
+
+zero_split_num = len(zero_split) - (bisect_right(zero_split, "") - bisect_left(zero_split, ""))
+one_split_num = len(one_split) - (bisect_right(one_split, "") - bisect_left(one_split, ""))
+
+answer = min(zero_split_num, one_split_num)
+print(answer)
